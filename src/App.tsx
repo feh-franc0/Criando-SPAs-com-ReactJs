4d4173f6f52a53b04f84832d503fa386763eb6ff
feh-franc0 <fernandofv1110@gmail.com>
2022-08-18 23:44:38 -0300

    Convertendo para contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-// import { Router } from "./Router";
-// import { BrowserRouter } from "react-router-dom";
-// import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-// import { GlobalStyle } from "./styles/global";
-// import { defaultTheme } from "./styles/themes/default";
-import { Home } from "./Home";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  return <Home />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //     </BrowserRouter>
-  //     <GlobalStyle />
-  //   </ThemeProvider>
-  // );
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
