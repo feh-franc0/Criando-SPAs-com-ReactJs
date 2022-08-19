8b7be0cc4aaaf8de2bec594373188bbeb70f1515
feh-franc0 <fernandofv1110@gmail.com>
2022-08-18 23:13:24 -0300

    Entendendo contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+// import { Router } from "./Router";
+// import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "styled-components";
 
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+// import { GlobalStyle } from "./styles/global";
+// import { defaultTheme } from "./styles/themes/default";
+import { Home } from "./Home";
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
-  );
+  return <Home />
+
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+  //     <GlobalStyle />
+  //   </ThemeProvider>
+  // );
 }
